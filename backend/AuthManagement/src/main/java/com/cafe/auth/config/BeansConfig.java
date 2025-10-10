8ed177d049852b63362fb95e41fcd3884b29414b
Karolina Sroczyk <karosroczyk@gmail.com>
2025-10-10 21:57:49 +0200

    standardizing applocation.properties file paths

--- backend/AuthManagement/src/main/java/com/cafe/auth/config/BeansConfig.java
+++ backend/AuthManagement/src/main/java/com/cafe/auth/config/BeansConfig.java
@@ -47,7 +47,8 @@ public class BeansConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+//        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.ORIGIN,
                 HttpHeaders.CONTENT_TYPE,
