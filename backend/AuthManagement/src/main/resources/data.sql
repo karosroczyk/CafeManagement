1cc81647d8dc6dc66102e990534c60bb9d2d8452
Karolina Sroczyk <karosroczyk@gmail.com>
2025-11-23 09:09:29 +0100

    change of database table name user -> users

--- backend/AuthManagement/src/main/resources/data.sql
+++ backend/AuthManagement/src/main/resources/data.sql
@@ -1,11 +1,11 @@
 -- USER --
-INSERT IGNORE INTO user (id, first_name, last_name, email, password, enabled, date_of_birth, account_locked, created_date, last_modified_date)
+INSERT IGNORE INTO users (id, first_name, last_name, email, password, enabled, date_of_birth, account_locked, created_date, last_modified_date)
 VALUES
 (1, 'client', 'client', 'client@gmail.com', '$2a$12$4Wbq6acQAeLTR5jPv6hwl.13LshFTZku5/fTtsKeViynOScattH0G', 1, '2000-08-05', 0, '2002-08-05', '2002-08-05');
-INSERT IGNORE INTO user (id, first_name, last_name, email, password, enabled, date_of_birth, account_locked, created_date, last_modified_date)
+INSERT IGNORE INTO users (id, first_name, last_name, email, password, enabled, date_of_birth, account_locked, created_date, last_modified_date)
 VALUES
 (2, 'employee', 'employee', 'employee@gmail.com', '$2a$12$CO8FI/khi7s8IylTF5tjJemz8j6iF.z/Tq/qLIoKm.Qpg3ndftTjm', 1, '2000-08-05', 0, '2002-08-05', '2002-08-05');
-INSERT IGNORE INTO user (id, first_name, last_name, email, password, enabled, date_of_birth, account_locked, created_date, last_modified_date)
+INSERT IGNORE INTO users (id, first_name, last_name, email, password, enabled, date_of_birth, account_locked, created_date, last_modified_date)
 VALUES
 (3, 'admin', 'admin', 'admin@gmail.com', '$2a$12$OkazfzU4Ux7.MuVKEknbFuoSMzAb.0gN7O4o7AqBN8dgxJXacKKfq', 1, '2000-08-05', 0, '2002-08-05', '2002-08-05');
 
